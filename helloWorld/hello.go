fc314caee5fc6b936dedfa87aa3c099009ce0206
Alex <asanchez1103@gmail.com>
2022-01-03 19:05:46 -0700

    Add language support to hello world
    Add subtests to handle each language choice.
    Refactor language handling into its own method.

--- helloWorld/hello.go
+++ helloWorld/hello.go
@@ -2,16 +2,38 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const german = "German"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const germanHelloPrefix = "Guten Tag, "
 const englishHelloDefaultSuffix = "World!!!"
 
-func Hello(name string) string {
+func Hello(name, language string) string {
+
 	if name == "" {
 		name = englishHelloDefaultSuffix
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case german:
+		prefix = germanHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Bub"))
+	fmt.Println(Hello("Human", ""))
 }
--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -10,15 +10,34 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	}
-	t.Run("Saying hello to someone (an argument is passed in.", func(t *testing.T) {
-		got := Hello("Bub")
+
+	t.Run("Saying hello to someone (an argument is passed in).", func(t *testing.T) {
+		got := Hello("Bub", "")
 		want := "Hello, Bub"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Saying hello to everyone (empty string passed in).", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!!!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In Spanish", func(t *testing.T) {
+		got := Hello("Josephina", "Spanish")
+		want := "Hola, Josephina"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In French", func(t *testing.T) {
+		got := Hello("Marcel", "French")
+		want := "Bonjour, Marcel"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In German", func(t *testing.T) {
+		got := Hello("Hilma", "German")
+		want := "Guten Tag, Hilma"
 		assertCorrectMessage(t, got, want)
 	})
 }
