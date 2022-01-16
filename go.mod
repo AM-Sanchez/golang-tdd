b7479bff912d9ec59077011ab281753e25fdebdb
Alex <asanchez1103@gmail.com>
2022-01-15 19:56:09 -0700

    Integers/ complete. Use github profile in go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/am-sanchez/golang-tdd
+
+go 1.17
---
+++ integers/adder.go
@@ -0,0 +1,7 @@
+// This package adds integers
+package integers
+
+// Add takes two integers and returns their sum.
+func Add(x, y int) int {
+	return x + y
+}
